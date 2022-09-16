54c7d0e02ce8041a454623cec9e232047c7bea59
12cmorrison12 <113653682+12cmorrison12@users.noreply.github.com>
2022-09-16 13:28:52 -0600

    Add files via upload

---
+++ num-exercise.js
@@ -0,0 +1,3 @@
+/* Write a function called randomGame that selects a random number between 0 and 1 every 1000 milliseconds and each time that a random number is picked, add 1 to a counter. 
+If the number is greater than .75, stop the timer and console.log the number of tries it took before we found a number greater than .75. */
+
