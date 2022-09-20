1ff7e87ab93189442faaa0e3ad932ecd34f7c7db
unknown <12cmorrison12@gmail.com>
2022-09-20 16:25:22 -0600

    second attempt

--- num-exercise.js
+++ num-exercise.js
@@ -2,9 +2,13 @@
 If the number is greater than .75, stop the timer and console.log the number of tries it took before we found a number greater than .75. */
 
 const randomGame = () => {
-    counter = setInterval(Math.random(), 1000);
-    i++;
-    if(counter > .75) {
-        console.log(counter);
-    }
+    let num = Math.round();
+    let count = 0;
+    let counter = setInterval(function() {
+        count++
+        if(counter > .75) {
+            clearInterval(counter);
+            console.log("Number of tries: " + count);
+        }
+    }, 1000);
 }
