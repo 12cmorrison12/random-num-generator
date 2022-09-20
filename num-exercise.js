399c6edaeda8ab2e5700d285432ece1b41d38c23
unknown <12cmorrison12@gmail.com>
2022-09-20 15:55:13 -0600

    first attempt

--- num-exercise.js
+++ num-exercise.js
@@ -1,3 +1,10 @@
 /* Write a function called randomGame that selects a random number between 0 and 1 every 1000 milliseconds and each time that a random number is picked, add 1 to a counter. 
 If the number is greater than .75, stop the timer and console.log the number of tries it took before we found a number greater than .75. */
 
+const randomGame = () => {
+    counter = setInterval(Math.random(), 1000);
+    i++;
+    if(counter > .75) {
+        console.log(counter);
+    }
+}
